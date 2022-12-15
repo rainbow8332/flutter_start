b95ff20d3f5a9cde2529cc75664923e53fd8b7b4
gukhwa <raindrop6651@gmail.com>
2022-12-15 18:13:48 +0900

    [add]bmi_app

---
+++ bmi_app/lib/home.dart
@@ -0,0 +1,354 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late int count;
+  late int count1;
+  late bool switchValue;
+  late String _buttonState;
+  late Color _color;
+  // late double _sliderValue;
+
+  @override
+  void initState() {
+    super.initState();
+    count = 10;
+    count1 = 40;
+    switchValue = false;
+    _buttonState = '남';
+    _color = Colors.deepPurple;
+    // _sliderValue = 170.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.purple,
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Text(
+                '당신의 BMI를 측정해보세요!',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    elevation: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Age(in Year)',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              '$count',
+                              style: const TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 30,
+                                child: FloatingActionButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        count--;
+                                      },
+                                    );
+                                  },
+                                  backgroundColor: Colors.purple,
+                                  child: const Icon(
+                                    Icons.remove,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                width: 30,
+                                child: FloatingActionButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        count++;
+                                      },
+                                    );
+                                  },
+                                  backgroundColor: Colors.purple,
+                                  child: const Icon(
+                                    Icons.add,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Card(
+                    elevation: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Weight(kg)',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              '$count1',
+                              style: const TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 30,
+                                child: FloatingActionButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        count1--;
+                                      },
+                                    );
+                                  },
+                                  backgroundColor: Colors.purple,
+                                  child: const Icon(
+                                    Icons.remove,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                width: 30,
+                                child: FloatingActionButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        count1++;
+                                      },
+                                    );
+                                  },
+                                  backgroundColor: Colors.purple,
+                                  child: const Icon(
+                                    Icons.add,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Card(
+                elevation: 10,
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20, bottom: 10),
+                      child: Text(
+                        'Height',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 20),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: Text(
+                            '170',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 40),
+                          ),
+                        ),
+                        Text(
+                          'Cm',
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    // Switch(value: switchValue, onChanged: (value) {})
+                    // Slider(
+                    //   value: _sliderValue,
+                    //   min: 0,
+                    //   max: 100,
+                    //   divisions: 20,
+                    //   onChanged: (value) {},
+                    // ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            _onClick();
+                          },
+                          child: Text(_buttonState),
+                          style:
+                              ElevatedButton.styleFrom(backgroundColor: _color),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'I`m',
+                              style: TextStyle(fontSize: 40),
+                            ),
+                            const SizedBox(
+                              width: 30,
+                            ),
+                            const Text(
+                              'Man',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            Switch(
+                              value: switchValue,
+                              onChanged: (value) {
+                                setState(() {
+                                  switchValue = value;
+                                  if (switchValue == false) {
+                                    _buttonState = '남';
+                                    _color = Colors.deepPurple;
+                                  } else {
+                                    _buttonState = '여';
+                                    _color = Colors.red;
+                                  }
+                                });
+                              },
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            const Text(
+                              'Woman',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: 45,
+              width: 100,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100))),
+                  child: const Text(
+                    '결과',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  )),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+
+  // ---------------------- Function ------------------------
+
+  _onClick() {
+    print("onClick()이 호출됨");
+    setState(() {
+      if (_buttonState == '남' &&
+          _color == Colors.deepPurple &&
+          switchValue == false) {
+        _buttonState = '여';
+        _color = Colors.red;
+        switchValue = true;
+      } else {
+        _buttonState = '남';
+        _color = Colors.deepPurple;
+        switchValue = false;
+      }
+    });
+    print(_buttonState);
+  }
+} // End
---
+++ bmi_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bmi_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+
